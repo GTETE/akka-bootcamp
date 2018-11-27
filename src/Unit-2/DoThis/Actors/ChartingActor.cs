f85edc6515188bf0c69942d97ff9db2b833eb8ab
GTETE <im.gtete@gmail.com>
2018-11-27 13:11:32 +0100

    Unit2 Lesson 2 Done !

--- src/Unit-2/DoThis/Actors/ChartingActor.cs
+++ src/Unit-2/DoThis/Actors/ChartingActor.cs
@@ -6,7 +6,7 @@ using Akka.Actor;
 
 namespace ChartApp.Actors
 {
-    public class ChartingActor : UntypedActor
+    public class ChartingActor : ReceiveActor
     {
         #region Messages
 
@@ -19,6 +19,21 @@ namespace ChartApp.Actors
 
             public Dictionary<string, Series> InitialSeries { get; private set; }
         }
+
+        /// <summary>
+        /// Add a new <see cref="Series"/> to the chart
+        /// </summary>
+        public class AddSeries
+        {
+            public AddSeries(Series series)
+            {
+                Series = series;
+            }
+
+            public Series Series { get; private set; }
+        }
+
+
 
         #endregion
 
@@ -33,16 +48,19 @@ namespace ChartApp.Actors
         {
             _chart = chart;
             _seriesIndex = seriesIndex;
+
+            Receive<InitializeChart>(ic => HandleInitialize(ic));
+            Receive<AddSeries>(addSeries => HandleAddSeries(addSeries));
         }
 
-        protected override void OnReceive(object message)
-        {
-            if (message is InitializeChart)
-            {
-                var ic = message as InitializeChart;
-                HandleInitialize(ic);
-            }
-        }
+        //protected override void OnReceive(object message)
+        //{
+        //    if (message is InitializeChart)
+        //    {
+        //        var ic = message as InitializeChart;
+        //        HandleInitialize(ic);
+        //    }
+        //}
 
         #region Individual Message Type Handlers
 
@@ -68,6 +86,18 @@ namespace ChartApp.Actors
                 }
             }
         }
+
+        private void HandleAddSeries(AddSeries series)
+        {
+            if (!string.IsNullOrEmpty(series.Series.Name) &&
+            !_seriesIndex.ContainsKey(series.Series.Name))
+            {
+                _seriesIndex.Add(series.Series.Name, series.Series);
+                _chart.Series.Add(series.Series);
+            }
+        }
+
+
 
         #endregion
     }
